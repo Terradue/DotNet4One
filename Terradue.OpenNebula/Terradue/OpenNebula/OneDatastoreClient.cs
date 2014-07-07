bff258398160dba2bfe71f44314a0bfe806879a1
Enguerran Boissier <enguerran.boissier@gmail.com>
2014-07-07 15:33:17 +0100

    add classes

---
+++ Terradue.OpenNebula/Terradue/OpenNebula/OneDatastoreClient.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using ServiceStack.Text;
+using CookComputing.XmlRpc;
+
+namespace Terradue.OpenNebula {
+
+    public class OneDatastoreClient : OneClient {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Terradue.OpenNebula.OneDatastoreClient"/> class.
+        /// </summary>
+        /// <param name="adminUsername">Admin username.</param>
+        /// <param name="adminPassword">Admin password.</param>
+        public OneDatastoreClient(string adminUsername, string adminPassword) : base(adminUsername, adminPassword){}
+
+        /// <summary>
+        /// Allocates the datastore.
+        /// </summary>
+        /// <returns>The datastore.</returns>
+        /// <param name="template">Template.</param>
+        public int AllocateDatastore(string template){
+            int result = 0;
+            XmlRpcDatastoreManagement xrum = XmlRpcProxyGen.Create<XmlRpcDatastoreManagement>();
+            Array openNebulaReturnArr = xrum.oneDatastoreAllocate(this.SessionSHA, template);
+            result = (int)openNebulaReturnArr.GetValue(1);
+            return result;
+        }
+
+        /// <summary>
+        /// Deletes the datastore.
+        /// </summary>
+        /// <returns><c>true</c>, if datastore was deleted, <c>false</c> otherwise.</returns>
+        /// <param name="dsId">Ds identifier.</param>
+        public bool DeleteDatastore(int dsId){
+            bool result = false;
+            XmlRpcDatastoreManagement xrum = XmlRpcProxyGen.Create<XmlRpcDatastoreManagement>();
+            Array openNebulaReturnArr = xrum.oneDatastoreDelete(this.SessionSHA, dsId);
+            result = (bool)openNebulaReturnArr.GetValue(0);
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the datastore info.
+        /// </summary>
+        /// <returns>The datastore info.</returns>
+        /// <param name="dsId">Ds identifier.</param>
+        public DATASTORE GetDatastoreInfo(int dsId){
+            DATASTORE result = null;
+            XmlRpcDatastoreManagement xrum = XmlRpcProxyGen.Create<XmlRpcDatastoreManagement>();
+            Array openNebulaReturnArr = xrum.oneDatastoreInfo(this.SessionSHA, dsId);
+            result = (DATASTORE)Deserialize(typeof(DATASTORE), openNebulaReturnArr.GetValue(1).ToString());
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the datastore list info.
+        /// </summary>
+        /// <returns>The datastore list info.</returns>
+        public DATASTORE_POOL GetDatastoreListInfo(){
+            DATASTORE_POOL result = null;
+            XmlRpcDatastoreManagement xrum = XmlRpcProxyGen.Create<XmlRpcDatastoreManagement>();
+            Array openNebulaReturnArr = xrum.oneDatastorePoolInfo(this.SessionSHA);
+            result = (DATASTORE_POOL)Deserialize(typeof(DATASTORE_POOL), openNebulaReturnArr.GetValue(1).ToString());
+            return result;
+        }
+
+        /// <summary>
+        /// Updates the datastore.
+        /// </summary>
+        /// <returns><c>true</c>, if datastore was updated, <c>false</c> otherwise.</returns>
+        /// <param name="dsId">Ds identifier.</param>
+        /// <param name="template">Template.</param>
+        /// <param name="type">Type.</param>
+        public bool UpdateDatastore(int dsId, string template, int type){
+            bool result = false;
+            XmlRpcDatastoreManagement xrum = XmlRpcProxyGen.Create<XmlRpcDatastoreManagement>();
+            Array openNebulaReturnArr = xrum.oneDatastoreUpdate(this.SessionSHA, dsId, template, type);
+            result = (bool)openNebulaReturnArr.GetValue(0);
+            return result;
+        }
+
+        /// <summary>
+        /// Change permissions og the Datastore
+        /// </summary>
+        /// <returns><c>true</c>, if chmod was datastored, <c>false</c> otherwise.</returns>
+        /// <param name="dsId">Ds identifier.</param>
+        /// <param name="userUse">User use.</param>
+        /// <param name="userManage">User manage.</param>
+        /// <param name="userAdmin">User admin.</param>
+        /// <param name="groupUse">Group use.</param>
+        /// <param name="groupManage">Group manage.</param>
+        /// <param name="groupAdmin">Group admin.</param>
+        /// <param name="otherUse">Other use.</param>
+        /// <param name="otherManage">Other manage.</param>
+        /// <param name="otherAdmin">Other admin.</param>
+        public bool DatastoreChmod(int dsId, int userUse, int userManage, int userAdmin, int groupUse, int groupManage, int groupAdmin, int otherUse, int otherManage, int otherAdmin){
+            bool result = false;
+            XmlRpcDatastoreManagement xrum = XmlRpcProxyGen.Create<XmlRpcDatastoreManagement>();
+            Array openNebulaReturnArr = xrum.oneDatastoreChmod(this.SessionSHA, dsId, userUse, userManage, userAdmin, groupUse, groupManage, groupAdmin, otherUse, otherManage, otherAdmin);
+            result = (bool)openNebulaReturnArr.GetValue(0);
+            return result;
+        }
+
+        /// <summary>
+        /// Change owner of the Datastore
+        /// </summary>
+        /// <returns><c>true</c>, if chown was datastored, <c>false</c> otherwise.</returns>
+        /// <param name="dsId">Ds identifier.</param>
+        /// <param name="userId">User identifier.</param>
+        /// <param name="groupId">Group identifier.</param>
+        public bool DatastoreChown(int dsId, int userId, int groupId){
+            bool result = false;
+            XmlRpcDatastoreManagement xrum = XmlRpcProxyGen.Create<XmlRpcDatastoreManagement>();
+            Array openNebulaReturnArr = xrum.oneDatastoreChown(this.SessionSHA, dsId, userId, groupId);
+            result = (bool)openNebulaReturnArr.GetValue(0);
+            return result;
+        }
+
+        /// <summary>
+        /// Renames the datastore.
+        /// </summary>
+        /// <returns><c>true</c>, if datastore was renamed, <c>false</c> otherwise.</returns>
+        /// <param name="dsId">Ds identifier.</param>
+        /// <param name="name">Name.</param>
+        public bool RenameDatastore(int dsId, string name){
+            bool result = false;
+            XmlRpcDatastoreManagement xrum = XmlRpcProxyGen.Create<XmlRpcDatastoreManagement>();
+            Array openNebulaReturnArr = xrum.oneDatastoreRename(this.SessionSHA, dsId, name);
+            result = (bool)openNebulaReturnArr.GetValue(0);
+            return result;
+        }
+
+    }
+}
+
---
+++ Terradue.OpenNebula/Terradue/OpenNebula/XmlRpc/XmlRpcDatastoreManagement.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using CookComputing.XmlRpc;
+
+namespace Terradue.OpenNebula {
+
+    [XmlRpcUrl(Terradue.OpenNebula.Configuration.XMLRPC_SERVER)]
+    public interface XmlRpcDatastoreManagement : IXmlRpcProxy
+    {
+
+
+        [XmlRpcMethod("one.datastore.allocate")]
+        Array oneDatastoreAllocate(string sessionSHA, string template);
+
+        /*Description: Allocates a new datastore in OpenNebula.
+
+            IN  String  The session string.
+            IN  String  A string containing the template of the datastore. Syntax can be the usual attribute=value or XML.
+            OUT Boolean     true or false whenever is successful or not
+            OUT Int/String  The allocated resource ID / The error string.
+            OUT Int     Error code.
+        */
+
+
+        [XmlRpcMethod("one.datastore.delete")]
+        Array oneDatastoreDelete(string sessionSHA, int datastoreId);
+
+        /*Description: Deletes the given datastore from the pool.
+
+            IN  String  The session string.
+            IN  Int     The object ID.
+            OUT Boolean     true or false whenever is successful or not
+            OUT Int/String  The resource ID / The error string.
+            OUT Int     Error code.
+        */
+
+
+        [XmlRpcMethod("one.datastore.info")]
+        Array oneDatastoreInfo(string sessionSHA, int datastoreId);
+
+        /*Description: Retrieves information for the datastore.
+
+            IN  String  The session string.
+            IN  Int     The object ID.
+            OUT Boolean     true or false whenever is successful or not
+            OUT String      The information string / The error string.
+            OUT Int     Error code.
+
+        */
+
+
+        [XmlRpcMethod("one.datastorepool.info")]
+        Array oneDatastorePoolInfo(string sessionSHA);
+
+        /*Description: Retrieves information for all or part of the datastores in the pool.
+
+            IN  String  The session string.
+            OUT Boolean     true or false whenever is successful or not
+            OUT Int/String  The information string / The error string.
+            OUT Int     Error code.
+        */
+
+
+        [XmlRpcMethod("one.datastore.update")]
+        Array oneDatastoreUpdate(string sessionSHA, int datastoreId, string template, int type);
+
+        /*Description: Replaces the datastore template contents.
+
+            IN  String  The session string.
+            IN  Int     The object ID.
+            IN  String  The new template contents. Syntax can be the usual attribute=value or XML.
+            IN  Int     Update type: 0: Replace the whole template. 1: Merge new template with the existing one.
+            OUT Boolean     true or false whenever is successful or not
+            OUT Int/String  The resource ID / The error string.
+            OUT Int     Error code.
+        */
+
+
+        [XmlRpcMethod("one.datastore.chmod")]
+        Array oneDatastoreChmod(string sessionSHA, int datastoreId, int userUse, int userManage, int userAdmin, int groupUse, int groupManage, int groupAdmin, int otherUse, int otherManage, int otherAdmin);
+
+        /*Description: Changes the permission bits of a datastore.
+
+            IN  String  The session string.
+            IN  Int     The object ID.
+            IN  Int     USER USE bit. If set to -1, it will not change.
+            IN  Int     USER MANAGE bit. If set to -1, it will not change.
+            IN  Int     USER ADMIN bit. If set to -1, it will not change.
+            IN  Int     GROUP USE bit. If set to -1, it will not change.
+            IN  Int     GROUP MANAGE bit. If set to -1, it will not change.
+            IN  Int     GROUP ADMIN bit. If set to -1, it will not change.
+            IN  Int     OTHER USE bit. If set to -1, it will not change.
+            IN  Int     OTHER MANAGE bit. If set to -1, it will not change.
+            IN  Int     OTHER ADMIN bit. If set to -1, it will not change.
+            OUT Boolean     true or false whenever is successful or not
+            OUT Int/String  The resource ID / The error string.
+            OUT Int     Error code.
+        */
+
+
+        [XmlRpcMethod("one.datastore.chown")]
+        Array oneDatastoreChown(string sessionSHA, int datastoreId, int userId, int groupId);
+
+        /*Description: Changes the ownership of a datastore.
+
+            IN  String  The session string.
+            IN  Int     The object ID.
+            IN  Int     The User ID of the new owner. If set to -1, the owner is not changed.
+            IN  Int     The Group ID of the new group. If set to -1, the group is not changed.
+            OUT Boolean     true or false whenever is successful or not
+            OUT Int/String  The resource ID / The error string.
+            OUT Int     Error code.
+        */
+
+
+        [XmlRpcMethod("one.datastore.rename")]
+        Array oneDatastoreRename(string sessionSHA, int datastoreId, string newName);
+
+        /*Description: Renames a datastore.
+
+            IN  String  The session string.
+            IN  Int     The object ID.
+            IN  String  The new name.
+            OUT Boolean     true or false whenever is successful or not
+            OUT Int/String  The VM ID / The error string.
+            OUT Int     Error code.
+        */
+
+    }
+}
+
