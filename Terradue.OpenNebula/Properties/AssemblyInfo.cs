180f16829f7a5ec26068d2507cac8a5fff746979
Emmanuel Mathot <emmanuel.mahot@gmail.com>
2014-07-31 17:41:17 +0200

    Version, description and nuget packages information
    Signed-off-by: Emmanuel Mathot <emmanuel.mahot@gmail.com>

--- Terradue.OpenNebula/Properties/AssemblyInfo.cs
+++ Terradue.OpenNebula/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Terradue.OpenNebula")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Terradue.DotNet4One is a library targeting .NET 4.0 and above providing an easy way to perform requests on any XML-RPC method exposed by an OpenNebula server.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Terradue")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("enguerran")]
+[assembly: AssemblyCopyright("Terradue")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
