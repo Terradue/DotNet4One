2df5aaf29388a6f80f30142e3db04dda2dc065bf
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-06-09 13:17:41 +0200

    license changed to AGPL
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.OpenNebula/Properties/AssemblyInfo.cs
+++ Terradue.OpenNebula/Properties/AssemblyInfo.cs
@@ -1,33 +1,45 @@
-﻿/*!
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+/*!
 
 \namespace Terradue.OpenNebula
 @{
-    DotNet4One Software Package
+    Terradue.OpenNebula module provides with a functonal layer to all OpenNebula functions via XML/RPC interface
 
     \xrefitem sw_version "Versions" "Software Package Version" 1.0.0
 
     \xrefitem sw_link "Link" "Software Package Link" [DotNet4One](https://github.com/Terradue/DotNet4One)
 
-    \xrefitem sw_license "License" "Software License" [GPLv3](https://github.com/Terradue/DotNet4One/blob/master/LICENSE)
+    \xrefitem sw_license "License" "Software License" [AGPL](https://github.com/Terradue/DotNet4One/blob/master/LICENSE)
 
     \ingroup Cloud
 @}
 
 */
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using NuGet4Mono.Extensions;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Terradue.OpenNebula")]
 [assembly: AssemblyDescription("DotNet4One is a library targeting .NET 4.0 and above providing an easy way to perform requests on any XML-RPC method exposed by an OpenNebula server.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Terradue")]
-[assembly: AssemblyProduct("Terradue.OpenNebula")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Terradue")]
-[assembly: AssemblyAuthors("Enguerran Boissier")]
-[assembly: AssemblyProjectUrl("https://github.com/Terradue/DotNet4One")]
-[assembly: AssemblyLicenseUrl("https://github.com/Terradue/DotNet4One/blob/master/LICENSE")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyInformationalVersion("1.0.2")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
